8a5adc8ba6302eb4e7aa4f34aa841c63bcc82d13
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2024-08-28 22:19:57 +0300

    Update Fc.py

--- Fc.py
+++ Fc.py
@@ -1,16 +1,14 @@
-import os, platform
+import os, platform, time, sys
+os.system('xdg-open https://wa.me/923065893752')
 try:
-    import requests
-except:
-    os.system('pip install requests')
-import requests
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
-if bit == '64bit':
-    from jutt import menu
-    menu()
+if bit == '64bit':                                                                                    print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m64Bit Found')
+ import jutt
 elif bit == '32bit':
-    from jutt import menu
-    menu()
-else:
-    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
-    os.system('exit')
+ print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m32Bit Found')
+ import jutt
